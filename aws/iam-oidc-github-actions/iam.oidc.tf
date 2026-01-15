23ffc06fd42df2bc746e5f84b0dca1e3dbb3d0cc
kbeckman <kbeckman@redfournine.com>
2026-01-14 22:56:18 -0500

    aws/iam-oidc-gha: adding GitHub certificate thumbprint

--- aws/iam-oidc-github-actions/iam.oidc.tf
+++ aws/iam-oidc-github-actions/iam.oidc.tf
@@ -1,8 +1,9 @@
 resource "aws_iam_openid_connect_provider" "default" {
   url = "https://token.actions.githubusercontent.com"
 
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = []
+  client_id_list = ["sts.amazonaws.com"]
+  # AWS auto-assigns this... If not provided here, it will appear as a diff for every plan/apply.
+  thumbprint_list = ["2b18947a6a9fc7764fd8b5fb18a863b0c6dac24f"]
 
   tags = local.default_tags
 }
