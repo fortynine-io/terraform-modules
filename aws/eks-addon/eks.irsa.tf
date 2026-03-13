dc1faed3436080052a37a6a75152d8fe3c4f6b17
Keith Beckman <kbeckman@redfournine.com>
2026-03-13 16:33:57 -0500

    aws/eks-addon: initial module implementation

---
+++ aws/eks-addon/eks.irsa.tf
@@ -0,0 +1,60 @@
+locals {
+  irsa_options = {
+    "amazon-cloudwatch-observability" = {
+      policy_arns = [
+        "arn:aws:iam::aws:policy/AWSXrayWriteOnlyAccess",
+        "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+      ]
+      service_account_name = "cloudwatch-agent"
+      namespace            = "amazon-cloudwatch"
+    }
+    "aws-ebs-csi-driver" = {
+      policy_arns          = ["arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"]
+      service_account_name = "ebs-csi-controller-sa"
+      namespace            = "kube-system"
+    }
+    "aws-efs-csi-driver" = {
+      policy_arns          = ["arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"]
+      service_account_name = "efs-csi-controller-sa"
+      namespace            = "kube-system"
+    }
+    "vpc-cni" = {
+      policy_arns          = ["arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"]
+      service_account_name = "aws-node"
+      namespace            = "kube-system"
+    }
+  }
+
+  irsa_config   = lookup(local.irsa_options, var.name, null)
+  requires_irsa = (lookup(local.irsa_options, var.name, null) != null) ? ["true"] : []
+}
+
+
+module "irsa" {
+  source = "git::https://github.com/fortynine-io/terraform-modules.git//aws/eks-irsa?ref=08a1187934c86750c0b33711fcdcd4b9a7389836"
+
+  for_each = toset(local.requires_irsa)
+
+  eks_cluster = {
+    name = var.cluster_name
+    oidc = var.cluster_oidc
+  }
+
+  description = "EKS Add-on (${var.name})"
+  name_slug   = local.irsa_config.service_account_name
+
+  trust_policy_subjects = {
+    exact_match = [
+      "system:serviceaccount:${local.irsa_config.namespace}:${local.irsa_config.service_account_name}"
+    ]
+  }
+
+  tags = local.default_tags
+}
+
+resource "aws_iam_role_policy_attachment" "irsa" {
+  for_each = toset(local.irsa_config.policy_arns)
+
+  role       = module.irsa.output.name
+  policy_arn = each.value
+}
---
+++ aws/eks-addon/main.tf
@@ -0,0 +1,29 @@
+locals {
+  module_version = trimspace(file("${path.module}/VERSION"))
+
+  default_tags = merge(var.tags, {
+    "ops/terraform-module" = "github.com/fortynine-io/terraform-modules/aws/eks-addon:${local.module_version}"
+  })
+}
+
+
+resource "aws_eks_addon" "addon" {
+  addon_name    = var.name
+  addon_version = var.addon_version
+  cluster_name  = var.cluster_name
+
+  configuration_values = var.configuration_values
+
+  preserve                    = false
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+  service_account_role_arn    = try(module.irsa["true"].arn, null)
+
+  timeouts {
+    create = var.timeouts.create
+    update = var.timeouts.update
+    delete = var.timeouts.delete
+  }
+
+  tags = local.default_tags
+}
