6daab71f99423b1a77df9a7216ce79ba62d36d45
Keith Beckman <kbeckman@redfournine.com>
2026-03-12 20:42:04 -0500

    aws/eks-irsa: renaming outputs to remove 'iam_role_' prefix

--- aws/eks-irsa/outputs.tf
+++ aws/eks-irsa/outputs.tf
@@ -1,14 +1,14 @@
-output "iam_role_arn" {
+output "arn" {
   description = "Kubernetes ServiceAccount IAM Role ARN."
   value       = aws_iam_role.irsa.arn
 }
 
-output "iam_role_description" {
+output "description" {
   description = "Kubernetes ServiceAccount IAM Role description."
   value       = aws_iam_role.irsa.description
 }
 
-output "iam_role_name" {
+output "name" {
   description = "Kubernetes ServiceAccount IAM Role name."
   value       = aws_iam_role.irsa.name
 }
