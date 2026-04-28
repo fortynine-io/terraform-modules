87e1a3d0d703c011f701decfc28e8fc248ac426d
kbeckman <kbeckman@redfournine.com>
2026-04-28 15:55:15 -0400

    refactor: removes kubernetes resources related to EKS Fargate (required dual-pass install)

--- aws/eks-cluster/eks.fargate.logging.tf
+++ aws/eks-cluster/eks.fargate.logging.tf
@@ -1,37 +1,5 @@
 # see: https://docs.aws.amazon.com/eks/latest/userguide/fargate-logging.html
 
-locals {
-  fargate_config_map_tpl = "${local.templates_path}/kubernetes/ConfigMap_fargate_logging.yaml.tpl"
-  fargate_logging_params = {
-    log_group_name = "/aws/eks/${var.name}/fargate"
-    region         = local.aws_region
-  }
-}
-
-
-resource "kubernetes_namespace_v1" "aws_observability" {
-  for_each = local.fargate_enabled_idx
-
-  metadata {
-    name = "aws-observability"
-
-    labels = {
-      "kubernetes.io/metadata.name" = "aws-observability"
-      "aws-observability"           = "enabled"
-    }
-  }
-}
-
-resource "kubernetes_manifest" "aws_observability_config_map" {
-  for_each = local.fargate_enabled_idx
-
-  manifest = yamldecode(templatefile(local.fargate_config_map_tpl, local.fargate_logging_params))
-
-  depends_on = [
-    kubernetes_namespace_v1.aws_observability
-  ]
-}
-
 # This policy differs from the official EKS Fargate logging documentation by disallowing the creation of a new Log
 # Group. This enforces Terraform control over the Fargate Log Group creation...
 resource "aws_iam_policy" "cloudwatch_fargate" {
--- aws/eks-cluster/versions.tf
+++ aws/eks-cluster/versions.tf
@@ -6,10 +6,6 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 6.0.0, < 7.0.0"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 3.1"
-    }
     tls = {
       source  = "hashicorp/tls"
       version = "~> 4.0"
